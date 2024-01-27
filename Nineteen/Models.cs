b38d829a2451213c55404a88d6ef1a216631b8ed
Petar Vukmirovic <petar.vukmirovic2@gmail.com>
2024-01-27 14:02:15 +0100

    first version of nineteen (builds)

---
+++ Nineteen/Models.cs
@@ -0,0 +1,50 @@
+﻿namespace Nineteen
+{
+    internal class Models
+    {
+        public record Part(int x, int m, int a, int s)
+        {
+            public int Rating => x + m + a + s;
+
+            public bool IsAccepted(Dictionary<string, Pipeline> pipelines)
+            {
+                var currentPipelineName = "in";
+                while (currentPipelineName != "A" && currentPipelineName != "R")
+                {
+                    var currentPipeline = pipelines[currentPipelineName];
+                    currentPipelineName = currentPipeline.FindResultingRule(this);
+                }
+                return currentPipelineName == "A";
+            }
+        }
+
+        public record Rule(Func<Part, int> propertySelector, Func<int, int, bool> comparator, int comparisonValue, string resultRule)
+        {
+            public bool IsSatisfied(Part part) => comparator(propertySelector(part), comparisonValue);
+
+            public static Rule OfStringDescription(string description)
+            {
+                var propertySelector = GetPropertySelector(description[0]);
+                Func<int, int, bool> comparator =
+                    description[1] == '<' ? (int a, int b) => a < b : (int a, int b) => a > b;
+                var comparisonValueAndResult = description[2..].Split(':');
+                return new Rule(propertySelector, comparator, int.Parse(comparisonValueAndResult[0]), comparisonValueAndResult[1]);
+            }
+
+            private static Func<Part, int> GetPropertySelector(char property) =>
+                property switch
+                {
+                    'x' => (Part p) => p.x,
+                    'm' => (Part p) => p.m,
+                    'a' => (Part p) => p.a,
+                    _ => (Part p) => p.s
+                };
+        }
+
+        public record Pipeline(string name, Rule[] rulesInPipline, string defaultRule)
+        {
+            public string FindResultingRule(Part part) =>
+                rulesInPipline.First(rule => rule.IsSatisfied(part)).resultRule;
+        }
+    }
+}
---
+++ Nineteen/Program.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using static Nineteen.Models;
+
+namespace Nineteen
+{
+    internal class Program
+    {
+        static (Dictionary<string, Pipeline> allPipelines, Part[] allParts) ParseInput()
+        {
+            var inputLines = Io.AllInputLines();
+            Dictionary<string, Pipeline> allPipelines = ParsePipelines(inputLines.TakeWhile(line => !string.IsNullOrEmpty(line)));
+            Part[] allParts = ParseParts(inputLines);
+            return (allPipelines, allParts);
+        }
+
+        static Dictionary<string, Pipeline> ParsePipelines(IEnumerable<string> inputLines)
+        {
+            var pipelines = new Dictionary<string, Pipeline>();
+            foreach (var line in inputLines)
+            {
+                var indexOfBrace = line.IndexOf('{');
+                var name = line[..indexOfBrace];
+                var rules = line[(indexOfBrace + 1)..(line.Length - 1)].Split(',', Io.IgnoreEmptyElements);
+                var nonDefaultRules = rules.Take(rules.Length - 1);
+                var defaultRule = rules.Last();
+                pipelines[name] = 
+                    new Pipeline(name, nonDefaultRules.Select(Rule.OfStringDescription).ToArray(), defaultRule);
+            }
+            return pipelines;
+        }
+
+        static Part[] ParseParts(IEnumerable<string> inputLines) =>
+            inputLines.Select(line =>
+            {
+                var partValues = line.Trim('{', '}').Split(',').Select(part => int.Parse(part[2..])).ToArray();
+                return new Part(partValues[0], partValues[1], partValues[2], partValues[3]);
+            }).ToArray();
+
+        static void PartOne()
+        {
+            var parsedInput = ParseInput();
+            var totalRating = parsedInput.allParts.Where(part => part.IsAccepted(parsedInput.allPipelines))
+                                                  .Sum(part => part.Rating);
+            Console.Write(totalRating);
+        }
+
+        static void Main(string[] args)
+        {
+            PartOne();
+        }
+    }
+}
